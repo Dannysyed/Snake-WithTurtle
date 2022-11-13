08fc592f956c59aba08463c30944f5138912c974
dannysyed <dannysyed20@gmail.com>
2022-11-14 00:54:08 +0530

    listen to keys

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from turtle import Turtle,Screen
 import time
 from snake import Snake
+snake=Snake() #IMPORTING THE CUSTOM CLASS
+
 ###### SCREEN SITTING ##########
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.tracer(0)
-# screen.bgcolor('black')
 screen.title('My Screen Game!')
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
 
-snake=Snake()
+
+###### GAME LOGIC ##########
 game_is_on=True
 while game_is_on:
     screen.update()
     time.sleep(0.1) 
     snake.move()
+    
    
  
-    
-
-
-
-
-
-
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,9 +1,14 @@
 from turtle import Turtle
 Starting_position=[(0,0),(-20,0),(-40,0)]
+Up=90
+Down=270
+Left=180
+Right=0
 class Snake:
     def __init__(self):
         self.segments=[]
         self.create_snake()
+        self.head=self.segments[0]
     
     def create_snake(self):
         for position in Starting_position:
@@ -18,5 +23,19 @@ class Snake:
             new_y=self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x,new_y)
         self.segments[0].forward(20)
+    def up(self):
+        if self.head.heading()!=Down :
+            self.segments[0].setheading(Up)
+    def down(self):
+       if self.head.heading()!=Up :      
+        self.head.setheading(Down)
+    def left(self):
+      if self.head.heading()!=Right :       
+        self.segments[0].setheading(Left)
+    def right(self):    
+       if self.head.heading()!=Left :  
+        self.segments[0].setheading(Right)
+       
+        
         
         
